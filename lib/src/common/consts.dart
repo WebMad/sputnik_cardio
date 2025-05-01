e31a05d9727b2def66b4b6d2a847a1255e5a17e7
gubin-dev <alecmei.gubin@yandex.ru>
2025-05-01 14:01:58 +0300

    fix leak

--- lib/src/common/consts.dart
+++ lib/src/common/consts.dart
@@ -1,5 +1,4 @@
 class Consts {
-  static const supabaseProjectURL = 'https://mgkfqnrgycthxgfjtqci.supabase.co';
-  static const supabaseApiKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1na2ZxbnJneWN0aHhnZmp0cWNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgwMzE0MDUsImV4cCI6MjA0MzYwNzQwNX0.TVmGju78qiaHpmDfotZ4GyAJajjUZZy8xqOap4Cj-Is';
+  static const supabaseProjectURL = String.fromEnvironment('SUPABASE_PROJECT_URL');
+  static const supabaseApiKey = String.fromEnvironment('SUPABASE_API_KEY');
 }
