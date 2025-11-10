e7de798f58a0c3964e391931bff155d0211d4fe4
Katherine <ekaterinathcubarkina@yandex.ru>
2025-11-10 13:01:32 +0300

    add backend server

---
+++ backend/services/sputnik_cardio_backend/test/sputnik_cardio_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:sputnik_cardio_backend/sputnik_cardio_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
